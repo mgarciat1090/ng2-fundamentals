10046574967f26ca91c80acc541fe2e7820294a9
Martin Garcia <jose.m.garcia.trejo@accenture.com>
2017-03-22 22:52:31 -0600

    Adding more code

---
+++ app/events/events-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core'
+import { EventService } from './shared/event.service'
+
+@Component({
+	selector : 'events-list',
+	template : `
+	<div>
+		<h1>Upcoming Angular 2 Events</h1>
+		<hr>
+		<div class="row">
+			<div *ngFor="let event of events" class="col-md-5">
+				<event-thumbnail  [event]="event"></event-thumbnail>
+			</div>
+		</div>
+	</div>
+	`
+})
+
+export class EventsListComponent {
+	events:any[];
+	constructor(private eventService: EventService){
+		
+	}
+
+	ngOnInit(){
+		this.events = this.eventService.getEvents()
+	}
+
+}
