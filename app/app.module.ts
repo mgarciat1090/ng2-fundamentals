2661803dae789df1a75bf77d81439ea2cfd7fe6a
Martin Garcia <jose.m.garcia.trejo@accenture.com>
2017-03-27 23:03:57 -0600

    Initial commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router'
 
 import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
@@ -7,16 +8,32 @@ import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
 
 import { EventService } from './events/shared/event.service'
+import { ToastrService } from './common/toastr.service'
+import { EventDetailsComponent } from './events/event-details/event-details.component'
+
+
+import { appRoutes } from './routes'
+import { CreateEventComponent } from './events/create-event.components'
+import { Error404Component } from './errors/404.component'
+import { EventRouteActivator } from './events/event-details/event-route-activator.service'
+
 
 @NgModule({
-	imports : [ BrowserModule ],
+	imports : [ 
+		BrowserModule,
+		RouterModule.forRoot(appRoutes)
+
+	 ],
 	declarations : [ 
 	EventsAppComponent,
 	EventsListComponent,
 	EventThumbnailComponent,
-	NavBarComponent
+	EventDetailsComponent,
+	NavBarComponent,
+	CreateEventComponent,
+	Error404Component
 	 ],
-	providers: [EventService],
+	providers: [EventService,ToastrService,EventRouteActivator],
 	bootstrap: [ EventsAppComponent ]
 
 })
